8df3f056a93b714284c1c63918ba8e55c6264542
saconov <svenehses@gmail.com>
2021-07-16 14:38:52 +0200

    added Menu Description and improved >Random Data generation in the table

---
+++ src/views/MenuView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-flex flex-column w-100">
+    <h3>Styles</h3>
+    <div class="">To change the Menue Style, adapt the <b>App.vue</b> as follows</div>
+    <div class="pb-3"> <b @click="$router.push('/router')" style="cursor:pointer"> Routercheck</b> to add Menu Entries</div>
+    <h4>Horizontal</h4>
+    <pre>
+     &lt;div id="<b>contentDividerHorizontal</b>"&gt;
+        &lt;div id="<b>menuSidebarHorizontal</b>"&gt;
+          &lt;<b>MenuHorizontal</b> /&gt;
+        &lt;/div>
+        &lt;div id="contendBar"&gt;
+          &lt;router-view /&gt;
+        &lt;/div&gt;
+      &lt;/div&gt;
+    </pre>
+    <h4>Vertical</h4>
+    <pre>
+       &lt;div id="<b>contentDividerVertical</b>"&gt;
+        &lt;div id="<b>menuSidebarVertical</b>"&gt;
+          &lt;<b>MenuVertical</b> /&gt;
+        &lt;/div>
+        &lt;div id="contendBar"&gt;
+          &lt;router-view /&gt;
+        &lt;/div&gt;
+      &lt;/div&gt;
+    </pre>
+  </div>
+</template>
--- src/views/TableCheck.vue
+++ src/views/TableCheck.vue
@@ -14,22 +14,24 @@
       />
     </div>
     <div class="props">
-        Properties
-        <ul>
-            <li>enable dark mode with attribute dark</li>
-            <li>paginated:"true"</li>
-           <li>fields:"fieldsDef" </li>
-           <li>tableData:"fieldData"</li>
-           <li>pageNumber:"pageNumber"</li>
-           <li>pageSize:"pagesize"</li>
-           <li>maxEntries:"maxResults"</li>
-      />
-        </ul>
+      Properties
+      <ul>
+        <li>enable dark mode with attribute dark</li>
+        <li>paginated:"true"</li>
+        <li>fields:"fieldsDef"</li>
+        <li>tableData:"fieldData"</li>
+        <li>pageNumber:"pageNumber"</li>
+        <li>pageSize:"pagesize"</li>
+        <li>maxEntries:"maxResults"</li>
+        />
+      </ul>
     </div>
     <div class="">
-        Emits:
-        <ul><li>@parameterChanged: Fired when pageSize has been changed </li>
-           <li>@pageChanged:Fired when pageNumber has been changed </li></ul>
+      Emits:
+      <ul>
+        <li>@parameterChanged: Fired when pageSize has been changed</li>
+        <li>@pageChanged:Fired when pageNumber has been changed</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -39,7 +41,7 @@ import { Options, Vue } from "vue-class-component";
 import dayjs, { Dayjs } from "dayjs";
 import { TableFields } from "@/components/table/types";
 import { generateField } from "@/components/table/tableutils";
-import GenericTable from "@/components/table/GenericTable.vue"
+import GenericTable from "@/components/table/GenericTable.vue";
 
 interface TestData {
   id: number;
@@ -52,17 +54,17 @@ interface TestData {
 }
 
 @Options({
-  components: {GenericTable},
+  components: { GenericTable },
 })
 export default class TableCheck extends Vue {
   fieldtestData: TestData[] = [];
-  displayedData:TestData[] = []
-  pageNumber = 1
-  pagesize = 10
-  maxResults = 1000
-  offset = 0
+  displayedData: TestData[] = [];
+  pageNumber = 1;
+  pagesize = 10;
+  maxResults = 1000;
+  offset = 0;
 
-   async updateTableAndFetchData(event: any) {
+  async updateTableAndFetchData(event: any) {
     this.pagesize = parseInt(event.pageSize);
     this.pageNumber = parseInt(event.pageNumber);
   }
@@ -73,29 +75,27 @@ export default class TableCheck extends Vue {
     tableFields.push(
       generateField<TestData>("Id", "id", {
         clickable: true,
-        clickfkt: (data: TestData) => { alert("click at "+data.id)},
+        clickfkt: (data: TestData) => {
+          alert("click at " + data.id);
+        },
       })
     );
     tableFields.push(
       generateField<TestData>("Code", "code", {
-        formatter: (data:TestData) => {return data.code.toLowerCase()}
+        formatter: (data: TestData) => {
+          return data.code.toLowerCase();
+        },
       })
     );
-    tableFields.push(
-      generateField<TestData>("Name", "name",{})
-    );
-    tableFields.push(
-      generateField<TestData>("Last Name", "last_name",{})
-    );
-    tableFields.push(
-      generateField<TestData>("Job", "job",{})
-    );
-    tableFields.push(
-      generateField<TestData>("Age", "age",{})
-    );
+    tableFields.push(generateField<TestData>("Name", "name", {}));
+    tableFields.push(generateField<TestData>("Last Name", "last_name", {}));
+    tableFields.push(generateField<TestData>("Job", "job", {}));
+    tableFields.push(generateField<TestData>("Age", "age", {}));
     tableFields.push(
       generateField<TestData>("Joined", "joined_at", {
-        formatter: (data:TestData) => {return data.joined_at.format("MM/DD/YYYY, hh:mm:ss")}
+        formatter: (data: TestData) => {
+          return data.joined_at.format("MM/DD/YYYY, hh:mm:ss");
+        },
       })
     );
 
@@ -103,66 +103,329 @@ export default class TableCheck extends Vue {
   }
 
   get fieldData(): TestData[] {
-      this.displayedData = this.fieldtestData
-      console.log(this.pagesize,this.pageNumber)
-    return this.displayedData.slice(this.pagesize*(this.pageNumber-1),this.pagesize*(this.pageNumber-1)+this.pagesize);
+    this.displayedData = this.fieldtestData;
+    console.log(this.pagesize, this.pageNumber);
+    return this.displayedData.slice(
+      this.pagesize * (this.pageNumber - 1),
+      this.pagesize * (this.pageNumber - 1) + this.pagesize
+    );
   }
 
   mounted() {
     const names = [
-      "Marc",
-      "John",
-      "Mario",
-      "David",
-      "Joe",
-      "Lisa",
-      "Marie",
-      "Ann",
-      "Joshua",
-      "Ben",
-      "Emma",
-      "Thomas",
-      "Patrick",
-      "Tim",
-      "Toby",
-      "Tom",
+     "Mayson", 	
+"Aadil", 	 	
+"Aubree", 	
+"Siobhan", 	
+"Uma", 	 	
+"Cleveland", 	
+"Finnian", 	
+"Bree", 	 	
+"Logan", 	 	
+"Andy", 	 	
+"Eve", 	 	
+"Cherry", 	
+"Marilyn", 	
+"Ifrah", 	 	
+"Angelo", 	
+"Ruby-Leigh", 
+"Kristen", 	
+"Ernie", 	 	
+"Donald", 	
+"Skylar", 	
+"Jarod", 	 	
+"Sumayya", 	
+"Pedro", 	 	
+"Darcey", 	
+"Buddy", 	 	
+"Manuel", 	
+"Clare", 	 	
+"Kavita", 	
+"Dani", 	 	
+"Aliya", 	 	
+"Maegan", 	
+"Kathleen", 	
+"Hibba", 	 	
+"Patience", 	
+"Kyron", 	 	
+"Neriah", 	
+"Maggie", 	
+"Jensen", 	
+"Colleen", 	
+"Julius", 	
+"Bessie", 	
+"Ayda", 	 	
+"Kelsi", 	 	
+"Kenzo", 	 	
+"Jaydon", 	
+"Adele", 	 	
+"Tye", 	 	
+"Roxanne", 	
+"Fannie", 	
+"Iris", 	 	
+"Kalum", 	 	
+"Precious", 	
+"Veronica", 	
+"Kaison", 	
+"Annalise", 	
+"Carl", 	 	
+"Yunus", 	 	
+"Chantal", 	
+"Aimee", 	 	
+"Radhika", 	
+"Bruce", 	 	
+"Priya", 	 	
+"Gareth", 	
+"Beauden", 	
+"Rome", 	 	
+"William", 	
+"Arian", 	 	
+"Christine", 	
+"Shelby", 	
+"Trent", 	 	
+"Reanna", 	
+"Glyn", 	 	
+"Giacomo", 	
+"Waleed", 	
+"Stefan", 	
+"Ayah", 	 	
+"Irene", 	 	
+"Rachelle", 	
+"Talia", 	 	
+"Sabrina", 	
+"Dalton", 	
+"Izabella", 	
+"Sierra", 	
+"Kameron", 	
+"Chloe", 	 	
+"Prisha", 	
+"Richie", 	
+"Jessica", 	
+"Jarrod", 	
+"Gracey", 	
+"Elsie", 	 	
+"Laibah", 	
+"Annette", 	
+"Riaan", 	 	
+"Boris", 	 	
+"Jamie-Leigh",
+"Huda", 	 	
+"Griffin", 	
+"Youssef", 	
+"Safiya", 	
     ];
-    const last_names = ["Miller","Miranda","Carrey","Jones","Cruise","Beck","Celso","Espinosa","Turk","Dorian","Canne","Curie","Hanks","Holland","Mahomes","Smith","Doe"];
+    const last_names = [
+      "Mora",
+"Salazar",
+"Roberts",
+"Medina",
+"Marin",
+"Moss",
+"Schultz",
+"Nixon",
+"Blacka",
+"Lindsey",
+"Velasquez",
+"Harvey",
+"Pollard",
+"Solomon",
+"Hines",
+"Bender",
+"Russell",
+"Flores",
+"Mcpherson",
+"Busby",
+"Kaiser",
+"Grimes",
+"Pritchard",
+"Silva",
+"Hodson",
+"Jordan",
+"Pena",
+"Bourne",
+"Rich",
+"Kearney",
+"Walls",
+"Morrison",
+"Everett",
+"Sherman",
+"Mair",
+"Rosario",
+"Pugh",
+"Mcmahon",
+"Leonard",
+"Kaye",
+"Rhodes",
+"Trevino",
+"Lovell",
+"Beltran",
+"Hahn",
+"Bellamy",
+"Huffman",
+"Ramsey",
+"Kim",
+"Sierra",
+"Boyle",
+"Liu",
+"Webster",
+"Cunningham",
+"Wardle",
+"Soto",
+"Lennon",
+"Moses",
+"Britt",
+"Whitehead",
+"Johns",
+"Rodgers",
+"Crawford",
+"Chase",
+"Dawe",
+"Watts",
+"Mohammed",
+"Forbes",
+"Jimenez",
+"Hawkins",
+"Ramsay",
+"Wilkes",
+"Poole",
+"Fry",
+"Dodd",
+"Guest",
+"Ball",
+"Ritter",
+"Talley",
+"Ray",
+"Bonilla",
+"Devlin",
+"Cannon",
+"Ellwood",
+"Clements",
+"Mcmillan",
+"Deleon",
+"Yates",
+"Richards",
+"Findlay",
+"Ortega",
+"O'Connor",
+"Thornton",
+"Griffin",
+"Bell",
+"Kouma",
+"Cuevas",
+"Cortes",
+"Gilmour",
+"Briggs"
+    ];
     const jobs = [
-      "IT",
-      "Marketing",
-      "Desk Job",
-      "Janitor",
-      "Pilot",
-      "Call Center",
-      "Smith",
-      "Butcher",
-      "Cashier",
-      "Farmer",
-      "Teacher",
-      "Professor",
-      "Firefighter",
-      "Police",
-      "Judge",
+      "Elementary School Teacher",
+      "Psychologist",
+      "Dancer",
+      "Telemarketer",
+      "Fitness Coach",
+      "Police Officer",
+      "Physicist",
+      "Recreational Therapist",
+      "Mechanical Engineer",
+      "Librarian",
+      "Desktop publisher",
+      "Radiologic Technologist",
+      "Patrol Officer",
+      "Mathematician",
+      "Musician",
+      "Reporter",
+      "Epidemiologist",
+      "Middle School Teacher",
+      "School Counselor",
       "Actor",
-      "Singer",
-      "Producer",
-      "Painter",
+      "Civil Engineer",
+      "Hairdresser",
+      "Art Director",
+      "Dental Hygienist",
+      "Educator",
+      "Security Guard",
+      "Automotive mechanic",
+      "Landscaper & Groundskeeper",
+      "Cost Estimator",
+      "Medical Secretary",
+      "Computer Hardware Engineer",
+      "Paramedic",
+      "Auto Mechanic",
+      "Photographer",
+      "Mason",
+      "Computer Support Specialist",
+      "Paralegal",
+      "Referee",
+      "Database administrator",
+      "Lawyer",
+      "Customer Service Representative",
+      "Occupational Therapist",
+      "Editor",
+      "Market Research Analyst",
+      "Clinical Laboratory Technician",
+      "Diagnostic Medical Sonographer",
+      "Receptionist",
+      "Budget analyst",
+      "Medical Assistant",
+      "Bookkeeping clerk",
+      "Public Relations Specialist",
+      "Social Work",
+      "Architect",
+      "Actuary",
+      "Software Developer",
+      "Urban Planner",
+      "Registered Nurse",
+      "Housekeeper",
+      "Bus Driver",
+      "Accountant",
+      "Electrical Engineer",
+      "Marketing Manager",
+      "Speech-Language Pathologist",
+      "Sports Coach",
+      "Designer",
+      "Substance Abuse Counselor",
+      "Insurance Agent",
+      "Professional athlete",
+      "Fitness Trainer",
+      "High School Teacher",
+      "Judge",
+      "Court Reporter",
+      "Dentist",
+      "Interpreter & Translator",
+      "Landscape Architect",
+      "Surveyor",
+      "Personal Care Aide",
+      "Physical Therapist",
+      "School Psychologist",
+      "Environmental scientist",
+      "Truck Driver",
+      "Chemist",
+      "Massage Therapist",
+      "Construction Manager",
+      "Maintenance  Work",
+      "Secretary",
+      "Food Scientist",
+      "Firefighter",
+      "Compliance Officer",
+      "Electrician",
     ];
-     let newfieldtestData: TestData[] = [];
+
+
+
+    let newfieldtestData: TestData[] = [];
     for (let i = 0; i < 1000; i++) {
       let testValue: TestData = {
         id: i,
         code: Math.random().toString(36).substring(7),
-        name: names[Math.floor(Math.random()*names.length)-1],
-        last_name : last_names[Math.floor(Math.random()*last_names.length)-1],
-        job:jobs[Math.floor(Math.random()*jobs.length)-1],
-        age: 18 +Math.ceil(Math.random()*60),
-        joined_at: dayjs().add(-Math.floor(Math.random()*9000),"day")
+        name: names[Math.floor(Math.random() * names.length) - 1],
+        last_name:
+          last_names[Math.floor(Math.random() * last_names.length) - 1],
+        job: jobs[Math.floor(Math.random() * jobs.length) - 1],
+        age: 18 + Math.ceil(Math.random() * 60),
+        joined_at: dayjs().add(-Math.floor(Math.random() * 9000), "day"),
       };
-      newfieldtestData.push(testValue)
+      newfieldtestData.push(testValue);
     }
-    this.fieldtestData = [...newfieldtestData]
+    this.fieldtestData = [...newfieldtestData];
   }
 }
 </script>
